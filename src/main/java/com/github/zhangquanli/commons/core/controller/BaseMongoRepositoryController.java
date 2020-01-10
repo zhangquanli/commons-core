48cfaced7b32795881f1aead0a275320cfa0192c
zhangquanli <502624709@qq.com>
2020-01-10 16:11:54 +0800

    增加 spring-data-mongodb 通用方法支持

--- src/main/java/com/github/zhangquanli/commons/core/controller/BaseMongoRepositoryController.java
+++ src/main/java/com/github/zhangquanli/commons/core/controller/BaseMongoRepositoryController.java
@@ -8,45 +8,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-public abstract class BaseJpaRepositoryController<T, ID, R extends JpaRepository<T, ID>> {
-    private R jpaRepository;
+public abstract class BaseMongoRepositoryController<T, ID, R extends MongoRepository<T, ID>> {
+    private R mongoRepository;
 
     @Autowired
-    public void setJpaRepository(R jpaRepository) {
-        this.jpaRepository = jpaRepository;
+    public void setMongoRepository(R mongoRepository) {
+        this.mongoRepository = mongoRepository;
     }
 
     @PostMapping
     @ApiOperation("新增")
     public ResponseResult<?> insert(T entity) {
-        jpaRepository.save(entity);
+        mongoRepository.save(entity);
         return ResponseResults.success();
     }
 
     @PutMapping
     @ApiOperation("根据主键修改")
     public ResponseResult<?> updateById(T entity) {
-        jpaRepository.save(entity);
+        mongoRepository.save(entity);
         return ResponseResults.success();
     }
 
     @DeleteMapping("/{id}")
     @ApiOperation("根据主键删除")
     public ResponseResult<?> deleteById(@PathVariable ID id) {
-        jpaRepository.deleteById(id);
+        mongoRepository.deleteById(id);
         return ResponseResults.success();
     }
 
     @GetMapping("/{id}")
     @ApiOperation("根据主键查询")
     public ResponseResult<T> findById(@PathVariable ID id) {
-        Optional<T> entityOptional = jpaRepository.findById(id);
+        Optional<T> entityOptional = mongoRepository.findById(id);
         return entityOptional.map(ResponseResults::success)
                 .orElseGet(ResponseResults::success);
     }
@@ -54,7 +54,7 @@ public abstract class BaseJpaRepositoryController<T, ID, R extends JpaRepository
     @GetMapping
     @ApiOperation("查询所有")
     public ResponseResult<List<T>> findAll() {
-        List<T> entities = jpaRepository.findAll();
+        List<T> entities = mongoRepository.findAll();
         return ResponseResults.success(entities);
     }
 
@@ -62,7 +62,7 @@ public abstract class BaseJpaRepositoryController<T, ID, R extends JpaRepository
     @ApiOperation("分页查询所有")
     public ResponseResult<PageData<T>> pageFindAll(@PathVariable Integer page, @PathVariable Integer size) {
         Pageable pageable = PageRequest.of(page - 1, size);
-        Page<T> entityPage = jpaRepository.findAll(pageable);
+        Page<T> entityPage = mongoRepository.findAll(pageable);
         return ResponseResults.success(entityPage);
     }
 }
